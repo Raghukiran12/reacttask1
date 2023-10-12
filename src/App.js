a7f7675fa151f423a2a8f0b7616fdac1ed82a6f3
Raghu Kiran <Raghu@raghukiran188@gmail.com>
2023-10-12 19:45:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {T1} from './Components_1/T1';
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+      
+      <T1 />
+  );
+}
+
+export default App;
